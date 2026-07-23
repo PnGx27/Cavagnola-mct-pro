e1c50f80d54f0e466e3e40467de1291e1b15fb11
PnGx27 <lilpeng69@gmail.com>
2026-07-23 23:40:25 +0200

    Update Service-worker.js

--- Service-worker.js
+++ Service-worker.js
@@ -4,7 +4,7 @@
 //             Network-first per Firebase
 // ═══════════════════════════════════════════════════
 
-const CACHE_NAME = "cavagnola-mct-v4";
+const CACHE_NAME = "cavagnola-mct-v5";
 
 
 // Asset da mettere in cache al primo install
